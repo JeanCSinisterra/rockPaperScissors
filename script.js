34cd44978ff189b118e193d8be9715f12cde7c50
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-07-11 07:34:25 -0500

    add files to rps-ui

--- script.js
+++ script.js
@@ -7,34 +7,104 @@ const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3)
     return choices[randomNumber]
 }
+
+// Initialize the Score
+let playerScore = 0;
+let computerScore = 0;
+
 // Function to play a round
 function playRound(playerSelection, computerSelection) {
+    
     // Make Player Selection case-insensitive
     playerSelection = playerSelection.toLowerCase();
     if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
         throw new Error('Invalid input for playerSelection');
     }
+
+    let result;
+
+    // Check the outcome of the round & update scores accordingly
     if (playerSelection === computerSelection) {
-        return "Tie";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper";
+        result =  "It's a Tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) 
+    {
+        result = "You Win!";
+        playerScore++;
     } else {
-        return "Look at you, You Lose against a Machine, Pathetic";
+        result = "You Lose!";
+        computerScore++;
+    }
+    // Update the Score Display
+    updateScore();
+
+    // Display the Result
+    displayResult(result);
+}
+
+// Function to update the Score display
+function updateScore() {
+    const playerScoreDisplay = document.querySelector("#player-score");
+    const computerScoreDisplay = document.querySelector("#computer-score");
+    playerScoreDisplay.textContent = `Player: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+
+    // Check if the Player has reached a score of 5 to Win the Game
+    if (playerScore === 5) {
+        displayResult.textContent = "Congratulations, You Beat Up a Machine! John Connor is proud";
+    }
+    else if (computerScore === 5) {
+        displayResult.textContent = "Look at you, You Lose against a Machine, Pathetic!";
     }
 }
 
-// Function to play the Game
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Enter your choice: rock, paper, or scissors');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+// Function to Display the Result
+function displayResult(result) {
+    const displayResult = document.querySelector("#score");
+    displayResult.textContent = result;
 }
+
+// Function to announce the Winner
+function announceWinner(winner){
+    const winnerDisplay = document.querySelector("#winner");
+    winnerDisplay.textContent = `The Winner is ${winner}!`;
+}
+
+// Function to Restarts the Game
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    // Update the Score Display
+    updateScore();
+    // Clear the Result Display
+    clearResult();
+    // Clear the Winner Display
+    clearWinner();
+}
+// Event listener for the Restart Button
+const restartButton = document.querySelector("#restart-game");
+restartButton.addEventListener("click", restartGame);
+
+// Event listener for the Buttons
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const playerSelection = e.target.dataset.selection;
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    })
+})
+
+// // Function to play the Game
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt('Enter your choice: rock, paper, or scissors');
+//         const computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
 
 // Testing the function 
 console.log(playRound('rock', 'scissors')); // should log "You Win! Rock beats Scissors"
